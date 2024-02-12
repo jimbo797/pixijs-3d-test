95e0988e03308c1c3fa31105214a96d1bc3a5b0d
Jimmy Kuhlman <jkuhl2022@gmail.com>
2024-02-11 19:21:04 -0500

    teapot on desk

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 /// <reference path='../global.d.ts' />
 
-import { Application, Assets, Renderer } from "pixi.js"
-import { CameraOrbitControl, LightingEnvironment, ImageBasedLighting, Model, Mesh3D, Light, LightType, ShadowCastingLight, ShadowQuality } from "pixi3d/pixi7"
+import { Application, Assets, Renderer, Sprite } from "pixi.js";
+import { CameraOrbitControl, LightingEnvironment, ImageBasedLighting, Model, Mesh3D, Light, LightType, ShadowCastingLight, ShadowQuality } from "pixi3d/pixi7";
+
 
 let app = new Application({
   backgroundColor: 0xdddddd, resizeTo: window, antialias: true
-})
-document.body.appendChild(app.view as HTMLCanvasElement)
+});
+document.body.appendChild(app.view as HTMLCanvasElement);
+app.stage.sortableChildren = true;
+
+
 
 const manifest = {
   bundles: [{
@@ -24,38 +28,54 @@ const manifest = {
         name: "teapot",
         srcs: "assets/teapot/teapot.gltf",
       },
+      {
+        name: "desk",
+        srcs: "assets/desk.jpg"
+      },
+      // {
+      //   name: "doodlebot",
+      //   srcs: "assets/Doodlebot/model.gltf"
+      // }
     ],
   }]
-}
+};
 
-await Assets.init({ manifest })
-let assets = await Assets.loadBundle("assets")
+await Assets.init({ manifest });
+let assets = await Assets.loadBundle("assets");
 
-let model = app.stage.addChild(Model.from(assets.teapot))
-model.y = -0.8
+let model = app.stage.addChild(Model.from(assets.teapot));
+model.y = -0.8;
 
-let ground = app.stage.addChild(Mesh3D.createPlane())
-ground.y = -0.8
-ground.scale.set(10, 1, 10)
+// let doodlebot = app.stage.addChild(Model.from(assets.doodlebot));
+
+// let ground = app.stage.addChild(Mesh3D.createPlane())
+// ground.y = -0.8
+// ground.scale.set(10, 1, 10)
+
+let backdrop = app.stage.addChild(Sprite.from(assets.desk));
+backdrop.y = -250;
+backdrop.zIndex = -10;
+backdrop.scale.set(.75, .8);
+
 
 LightingEnvironment.main.imageBasedLighting = new ImageBasedLighting(
   assets.diffuse,
   assets.specular
-)
+);
 
-let directionalLight = new Light()
-directionalLight.intensity = 1
-directionalLight.type = LightType.directional
-directionalLight.rotationQuaternion.setEulerAngles(25, 120, 0)
-LightingEnvironment.main.lights.push(directionalLight)
+let directionalLight = new Light();
+directionalLight.intensity = 1;
+directionalLight.type = LightType.directional;
+directionalLight.rotationQuaternion.setEulerAngles(25, 120, 0);
+LightingEnvironment.main.lights.push(directionalLight);
 
 let shadowCastingLight = new ShadowCastingLight(
-  app.renderer as Renderer, directionalLight, { shadowTextureSize: 1024, quality: ShadowQuality.medium })
-shadowCastingLight.softness = 1
-shadowCastingLight.shadowArea = 15
+  app.renderer as Renderer, directionalLight, { shadowTextureSize: 1024, quality: ShadowQuality.medium });
+shadowCastingLight.softness = 1;
+shadowCastingLight.shadowArea = 15;
 
-let pipeline = app.renderer.plugins.pipeline
-pipeline.enableShadows(ground, shadowCastingLight)
-pipeline.enableShadows(model, shadowCastingLight)
+let pipeline = app.renderer.plugins.pipeline;
+// pipeline.enableShadows(ground, shadowCastingLight)
+pipeline.enableShadows(model, shadowCastingLight);
 
 let control = new CameraOrbitControl(app.view as HTMLCanvasElement);
